dae8ee737eb5a5a694bf545fd13e5a04ff72dd33
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-05-08 17:53:19 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  has_one :order
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :shipping_day_id, :prefecture_id, :image).merge(user_id: current_user.id)
+  end
 end
