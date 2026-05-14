ee0cec908be176add56057adf155a8b319571c92
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-05-14 15:54:04 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
