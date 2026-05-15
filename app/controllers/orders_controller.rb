a86c93bbae67b0eeba99d11195a1d95c17686743
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-05-15 18:57:40 +0900

    コントローラーにアクションの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,42 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:new, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @orders = Order.includes(:item).where(user_id: current_user.id)
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def order_params
+    params.require(:order_address).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :house_number,
+      :building_name,
+      :phone_number
+    ).merge(
+      user_id: current_user.id,
+      item_id: params[:item_id]
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
+
   end
 end
