1e6ed1f4bb86493b3068188275897d07b5c9fe60
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-05-16 16:13:23 +0900

    クレジットカードの決済機能の導入

---
+++ app/javascript/card.js
@@ -0,0 +1,33 @@
+const pay = () => {
+  if (!document.getElementById("number-form")) return;
+
+  const payjp = Payjp(window.payjpPublicKey);
+  const elements = payjp.elements();
+
+  const numberElement = elements.create("cardNumber");
+  const expiryElement = elements.create("cardExpiry");
+  const cvcElement = elements.create("cardCvc");
+
+  numberElement.mount("#number-form");
+  expiryElement.mount("#expiry-form");
+  cvcElement.mount("#cvc-form");
+
+  const form = document.getElementById("charge-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    payjp.createToken(numberElement).then((response) => {
+      if (response.error) {
+        alert(response.error.message);
+      } else {
+        const token = response.id;
+        document.getElementById("card-token").value = token;
+        form.submit();
+      }
+    });
+  });
+
+};
+
+window.addEventListener("turbo:load", pay);
