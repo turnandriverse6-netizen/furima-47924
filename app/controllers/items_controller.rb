f9e7ca57af6887e130e0b7c66a08ae73f7f3158b
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-05-21 17:58:49 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,9 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user == @item.user
-    redirect_to root_path
-    else @item.order.present?
-    redirect_to root_path
+    if current_user != @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 end
