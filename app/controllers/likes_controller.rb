15e757fba404c3ea2704d346ffac56c079320ff9
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-06-16 16:41:15 +0900

    出品者本人がいいねできないようにコントローラーで制御

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,10 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.likes.create(item_id: params[:item_id])
-    redirect_to item_path(params[:item_id])
+    item = Item.find(params[:item_id])
+    return redirect_to item_path(item) if item.user_id == current_user.id
+    current_user.likes.find_or_create_by(item_id: item.id)
+    redirect_to item_path(item)
   end
 
   def destroy
