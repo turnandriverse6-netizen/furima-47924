00a7bf22db8d84d0cc8223e6152d2458a64a2619
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-06-16 16:13:40 +0900

    likesコントローラーの設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    current_user.likes.create(item_id: params[:item_id])
+    redirect_to item_path(params[:item_id])
+  end
+
+  def destroy
+    current_user.likes.find_by(item_id: params[:item_id])&.destroy
+    redirect_to item_path(params[:item_id])
+  end
 end
