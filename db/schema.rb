ea5ceb43294f3187852ac89936c649921cf2810d
turnandriverse6-netizen <turnandriverse6@gmail.com>
2026-06-17 11:17:43 +0900

    コメントテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_16_065033) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_17_021704) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,14 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_16_065033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_addresses_on_order_id"
+  end
+
+  create_table "comments", charset: "utf8mb3", force: :cascade do |t|
+    t.text "text"
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
